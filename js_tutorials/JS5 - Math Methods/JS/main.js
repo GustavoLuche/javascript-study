f6ab7c4d6af7d6074278528803bf8d491281826e
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-04-27 00:14:44 -0300

    Add JS5 - Math Methods

---
+++ js_tutorials/JS5 - Math Methods/JS/main.js
@@ -0,0 +1,22 @@
+// Math Methods and Properties
+console.log(Math.PI)
+console.log(Math.round(Math.PI))
+console.log(Math.ceil(Math.PI))
+console.log(Math.floor(Math.PI))
+
+console.log(Math.pow(2, 4))
+
+console.log(Math.min(2, 0.5, 9))
+console.log(Math.max(2, 0.5, 9))
+
+console.log(Math.random())
+
+// From 1 to 10
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
