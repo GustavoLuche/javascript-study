6e4936527c529f28ee214a723648f3fa868e44fd
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-04-27 00:30:26 -0300

    Add JS6 - Code Challenge

---
+++ js_tutorials/JS6 - Code Challenge/JS/main.js
@@ -0,0 +1,13 @@
+// Your First Code Challenge
+
+// Write code that will return a random letter from your name
+
+// Let's make this work for ALL names
+const anyName = "Jonathan";
+
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
